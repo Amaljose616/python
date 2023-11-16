b0ffb1b500b21be5b27eef59c36d6c01ab478432
Amal Jose <amal.jose@vonnue.com>
2023-11-16 20:48:57 +0530

    Import modules in python

---
+++ sample_module.py
@@ -0,0 +1,10 @@
+def checkNegorPositive(num):
+    if num < 0:
+        print("The number is a negative number")
+    elif num == 0:
+        print("The number is zero")
+    else:
+        print("The number is a positive number")
+
+
+
